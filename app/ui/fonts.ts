c8dc9f68193562a53230f41bdd34cfcb634b5638
Zak <mjanwarsi@gmail.com>
2025-01-06 12:27:28 +0500

    fixed fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,5 @@
 
 import { Inter, Lusitana, Modak } from "next/font/google";
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({ weight: "400" });
-export const modak = Modak({ weight: "400" });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
+export const modak = Modak({ weight: "400", subsets: ["latin"] });
